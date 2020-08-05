3ae21e41ff984ea4437e86ff0fc8e4543a4fcdcb
wiliancalora <wiliancalora@evoluaeducacao.com.br>
2020-08-05 06:10:54 -0300

    Finalização do desafio

--- src/components/Food/index.tsx
+++ src/components/Food/index.tsx
@@ -4,6 +4,8 @@ import { FiEdit3, FiTrash } from 'react-icons/fi';
 
 import { Container } from './styles';
 
+import api from '../../services/api';
+
 interface IFoodPlate {
   id: number;
   name: string;
@@ -27,11 +29,23 @@ const Food: React.FC<IProps> = ({
   const [isAvailable, setIsAvailable] = useState(food.available);
 
   async function toggleAvailable(): Promise<void> {
-    // TODO UPDATE STATUS (available)
+    try {
+      // TODO UPDATE STATUS (available)
+      await api.put(`/foods/${food.id}`, {
+        ...food,
+        available: !isAvailable,
+      });
+
+      setIsAvailable(!isAvailable);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   function setEditingFood(): void {
     // TODO - SET THE ID OF THE CURRENT ITEM TO THE EDITING FOOD AND OPEN MODAL
+    handleEditFood(food);
+    console.log(food);
   }
 
   return (
